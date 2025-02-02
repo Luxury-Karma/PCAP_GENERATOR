494daf149edffb5699b58576f6c3589d82014be7
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2025-02-02 15:23:22 -0500

    removal of unuse function and clarification

--- moduals/AI_communication.py
+++ moduals/AI_communication.py
@@ -4,13 +4,6 @@ class OllamaClient:
     def __init__(self, model='llama3'):
         self.model = model
 
-    def chat(self, user_message):
-        response = ollama.chat(
-            model=self.model,
-            messages=[{'role': 'user', 'content': user_message}]
-        )
-        return response['message']['content']
-
     def generate_text(self, prompt):
         result = ollama.generate(
             model=self.model,
