ec49821a4a67417e011797f99bc760fc718aaeba
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2025-02-03 18:47:30 -0500

    added Ping and tracert option
    Allow the usage of ping and tracert and ping for the script.
    
    We will use this to add noise and look like someone is pocking in the network

--- moduals/icmp.py
+++ moduals/icmp.py
@@ -1,1 +1,17 @@
 #TODO: Behing able to simulate a NMAP scan if wanted. making ICMP noise from a couple devices ( potentially need to use SSH who know we'll do it once there )
+import subprocess
+
+
+def ping(dest:str, ping_amount:int = 4):
+    try:
+        subprocess.run(f'ping {dest} -c {ping_amount}', shell=True)
+    except:
+        print(f"was not able to ping destination {dest}")
+
+def tracert(dest:str):
+    try:
+        subprocess.run(f'traceroute {dest}',shell=True)
+    except:
+        print(f"Was not able to find a route to {dest}")
+
+
