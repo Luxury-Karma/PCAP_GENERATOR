981fe39e4cc78f64240fbcba61956de57c950be2
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2025-02-04 14:32:45 -0500

    FTP connection work

--- moduals/ssh.py
+++ moduals/ssh.py
@@ -12,7 +12,21 @@ def connect_to_ssh_server(ssh_server_ip:str,user_name:str,password:str,port:int=
     except:
         return ssh, False
 
-def send_command_to_shell(shell:SSHClient,command:str):
-    shell.exec_command(command)
+
+def send_command_to_shell(shell: paramiko.SSHClient, command: str):
+    stdin, stdout, stderr = shell.exec_command(command)
+
+    exit_status = stdout.channel.recv_exit_status()  # Get exit status
+    output = stdout.read().decode().strip()  # Read standard output
+    error = stderr.read().decode().strip()  # Read standard error
+
+    if exit_status == 0:
+        print(f"Success: {output}")
+        return True, output  # Indicating success
+    else:
+        print(f"Error: {error}")
+        return False, error  # Indicating failure
+
 
 #TODO: Add the bruteforce thing here. We could also use Hydra command line from a kali machine that we connect through SSH?
+
